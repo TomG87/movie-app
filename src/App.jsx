80f796d21af2cb17fddd390599a41d5614c14ca7
Tom Gagliano <tgaglianoll@gmail.com>
2023-07-21 17:42:05 -0400

    add movie app to app.jsx file and create app.css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
+import { FcSearch } from "react-icons/fc";
 
-function App() {
-  const [count, setCount] = useState(0)
+const API_URL = "http://www.omdbapi.com?apikey=";
+//
+
+const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("X-Men");
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <h1>MovieUniverse</h1>
+      <div className="search">
+        <input
+          placeholder="search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <FcSearch alt="search" onClick={() => {}} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className="container"></div>
+      <div className="movie"></div>
+    </div>
+  );
+};
 
-export default App
+export default App;
