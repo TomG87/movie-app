0e790413fddc0c0be5973dd4c57de867016fb085
Tom Gagliano <tgaglianoll@gmail.com>
2023-07-22 12:31:11 -0400

    add MovieCard.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { FcSearch } from "react-icons/fc";
+import { MovieCard } from "./MovieCard";
 
 const API_URL = "http://www.omdbapi.com?apikey=";
 //
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search);
+    setMovies(data.Search);
   };
 
   useEffect(() => {
@@ -27,8 +29,18 @@ const App = () => {
         />
         <FcSearch alt="search" onClick={() => {}} />
       </div>
-      <div className="container"></div>
-      <div className="movie"></div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 };
